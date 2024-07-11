ab9c83a068700f2359424c6e70e8f398d0077815
halbykevin <halbykevin@hotmail.com>
2024-07-12 00:21:31 +0300

    changing to new buttons and logo in every page

--- myPoints.php
+++ myPoints.php
@@ -95,14 +95,65 @@ display: none; /* Hide the open button initially */
           display: block; /* Show the open button on small screens */
         }
       }
+      .logo {
+        display: block;
+        margin: 20px auto;
+        width: 300px;
+        height: auto;
+      }
+      .button-container .btn {
+        font-size: 17px;
+        background: transparent;
+        border: none;
+        padding: 1em 1.5em;
+        color: black;
+        text-transform: uppercase;
+        position: relative;
+        transition: 0.5s ease;
+        cursor: pointer;
+      }
+      .button-container .btn::before {
+        content: "";
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        height: 2px;
+        width: 0;
+        background-color: #ffc506;
+        transition: 0.5s ease;
+      }
+      .button-container .btn:hover {
+        color: #1e1e2b;
+        transition-delay: 0.5s;
+      }
+      .button-container .btn:hover::before {
+        width: 100%;
+      }
+      .button-container .btn::after {
+        content: "";
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        height: 0;
+        width: 100%;
+        background-color: #ffc506;
+        transition: 0.4s ease;
+        z-index: -1;
+      }
+      .button-container .btn:hover::after {
+        height: 100%;
+        transition-delay: 0.4s;
+      }
     </style>
+    <img src="uploads/logo.png" class="logo" alt="logo" />
+
     <div class="button-container">
-      <button onclick="location.href='index.html'">Home</button>
-      <button onclick="location.href='discover.php'">Discover</button>
-      <button onclick="location.href='liked.php'">Liked</button>
-      <button onclick="location.href='myPoints.php'">My Points</button>
-      <button onclick="logout()">Logout</button>
-    </div>
+    <button class="btn" onclick="location.href='index.html'">Home</button>
+    <button class="btn" onclick="location.href='discover.php'">Discover</button>
+    <button class="btn" onclick="location.href='liked.php'">Liked</button>
+    <button class="btn" onclick="location.href='myPoints.php'">My Points</button>
+    <button class="btn" onclick="logout()">Logout</button>
+</div>
 
     <button class="openbtn" onclick="openNav()">☰</button>
 
